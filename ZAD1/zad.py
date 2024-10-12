91b162b204afb3b115446df9af3c9c7c6e77748c
Jan Zajac <138476188+Szogunisko@users.noreply.github.com>
2024-10-12 16:27:05 +0200

    Update zad.py

--- ZAD1/zad.py
+++ ZAD1/zad.py
@@ -5,93 +5,117 @@ import csv
 from matplotlib.lines import Line2D
 from matplotlib.legend_handler import HandlerLine2D
 
+#Ustawienei czcionki
+plt.rc('font', family='serif', serif='Times New Roman')
+
+# Dane dla 1-Evol_RS
 with open('results/rsel.csv', 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
-data_array = np.array(data)
-data_array = np.array(data_array[1:], dtype=float)
-data_y = np.mean(data_array[:, 2:], axis=1)
-x11 = data_array[:,1]
-x12= data_array[:,0]
-y1 = data_y
+data_rsel = np.array(data)
+data_rsel = np.array(data_rsel[1:], dtype=float)
+x1 = data_rsel[:,1]
+y1 = np.mean(data_rsel[:, 2:], axis=1)
 # Dane dla 1-Coev_RS
 with open('results/cel-rs.csv', 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
-data_array = np.array(data)
-data_array = np.array(data_array[1:], dtype=float)
-data_y = np.mean(data_array[:, 2:], axis=1)
-x21 = data_array[:,1]
-x22= data_array[:,0]
-y2 = data_y
+data_cel_rs = np.array(data)
+data_cel_rs = np.array(data_cel_rs[1:], dtype=float)
+x2 = data_cel_rs[:,1]
+y2 = np.mean(data_cel_rs[:, 2:], axis=1)
 # Dane dla 2-Coev_RS
 with open('results/2cel-rs.csv', 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
-data_array = np.array(data)
-data_array = np.array(data_array[1:], dtype=float)
-data_y = np.mean(data_array[:, 2:], axis=1) 
-x31 = data_array[:,1]
-x32= data_array[:,0]
-y3 = data_y
+data_2cel_rs = np.array(data)
+data_2cel_rs = np.array(data_2cel_rs[1:], dtype=float)
+x3 = data_2cel_rs[:,1]
+y3 = np.mean(data_2cel_rs[:, 2:], axis=1) 
 # Dane dla 1-Coev
 with open('results/cel.csv', 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
-data_array = np.array(data)
-data_array = np.array(data_array[1:], dtype=float)
-data_y = np.mean(data_array[:, 2:], axis=1)
-x41 = data_array[:,1]
-x42= data_array[:,0]
-y4 = data_y
+data_cel = np.array(data)
+data_cel = np.array(data_cel[1:], dtype=float)
+x4 = data_cel[:,1]
+y4 = np.mean(data_cel[:, 2:], axis=1)
 # Dane dla 2-Coev
 with open('results/2cel.csv', 'r') as f:
     reader = csv.reader(f)
     data = list(reader)
-data_array = np.array(data)
-data_array = np.array(data_array[1:], dtype=float)
-data_y = np.mean(data_array[:, 2:], axis=1)
-x51 = data_array[:,1]
-x52= data_array[:,0]
-y5 = data_y # Przykładowe dane dla 1-Evol-RS
+data_2cel = np.array(data)
+data_2cel = np.array(data_2cel[1:], dtype=float)
+x5 = data_2cel[:,1]
+y5 = np.mean(data_2cel[:, 2:], axis=1)
 
-# Create a figure with two subplots (one row, two columns)
-fig, (ax1, box1) = plt.subplots(1, 2, figsize=(6.7, 4.7))
+# Stworzenie pola z dwoma wykresami: jeden wiersz, dwie kolumny, szerokosc i wysokosc w calach
+fig, (ax1, box1) = plt.subplots(1, 2, figsize=(6.7, 5))
 
-# Plot the first line chart (on the left)
-line1, = ax1.plot(x11/1000, y1*100, label='1-Evol-RS', color='blue', marker='o', markevery=25)
-ax1.plot(x21/1000, y2*100, label='1-Coev-RS', color='green', marker='v', markevery=25)
-ax1.plot(x31/1000, y3*100, label='2-Coev-RS', color='red', marker='D', markevery=25)
-ax1.plot(x41/1000, y4*100, label='1-Coev', color='black', marker='s', markevery=25)
-ax1.plot(x51/1000, y5*100, label='2-Coev', color='magenta', marker='d', markevery=25)
+# Pierwszy wykres liniowy z markerami
+ax1.plot(x1/1000, y1*100, label='1-Evol-RS', color='blue', marker='o', markevery=25, markeredgecolor = 'black', markeredgewidth=0.5,alpha=0.8,linewidth=0.75)
+ax1.plot(x2/1000, y2*100, label='1-Coev-RS', color='green', marker='v', markevery=25, markeredgecolor = 'black', markeredgewidth=0.5,alpha=0.8,linewidth=0.75)
+ax1.plot(x3/1000, y3*100, label='2-Coev-RS', color='red', marker='D', markevery=25, markeredgecolor = 'black', markeredgewidth=0.5,alpha=0.8,linewidth=0.75)
+ax1.plot(x4/1000, y4*100, label='1-Coev', color='black', marker='s', markevery=25, markeredgecolor = 'black', markeredgewidth=0.5,alpha=0.8,linewidth=0.75)
+ax1.plot(x5/1000, y5*100, label='2-Coev', color='magenta', marker='d', markevery=25, markeredgecolor = 'black', markeredgewidth=0.5,alpha=0.8,linewidth=0.75)
 ax1.set_xlim(0,500)
 ax1.set_ylim(60,100)
-plt.margins(x=0, y=0)
-ax1.set_xlabel('Rozegranych gier (x1000)')
+ax1.tick_params(axis='x', direction='in', length=3)
+ax1.tick_params(axis='y', direction='in', length=3)
+#ustawienie znacznikow y po dwóch stronach
+ax1.yaxis.set_ticks_position('both')
+ax1.set_xlabel('Rozegranych gier (×1000)')
 ax1.set_ylabel('Odsetek wygranych gier [%]')
-ax1.grid(linestyle = '--')
-ax1.legend(loc='lower right', handler_map={line1: HandlerLine2D(numpoints=2)})
+ax1.grid(linestyle = '--', linewidth=0.25, dashes=(7, 14))
+ax1.legend(loc='lower right', numpoints=2)
 
-#TUTEJ
 ax2 = ax1.twiny()
 ax2.set_xlim(0,200)
+#usuniecie zewnetrznych znacznikow
+ax2.tick_params(axis='x', direction='in', length=3)
+ax2.tick_params(axis='y', direction='in', length=3)
 top_x = np.linspace(0, 200, 40)
 ax2.set_xticks(np.arange(0, 201, 40))
 ax2.set_xlabel('Pokolenie')
 
 
-# Create the boxplot on the second subplot (on the right)
-data_boxplot = [y1,y2,y3,y4,y5]
-box1.boxplot(data_boxplot, notch=True, showmeans=True, patch_artist=False)
+# Dane do boxplota
+boxplot_data = [
+    data_rsel[-1, 2:]*100,   
+    data_cel_rs[-1, 2:]*100, 
+    data_2cel_rs[-1, 2:]*100,
+    data_cel[-1, 2:]*100,    
+    data_2cel[-1, 2:]*100   
+]
 
-# Add labels and grid to the second plot
-box1.set_xticklabels(['1-Evol-RS', '1-Coev-RS', '2-Coev-RS', '1-Coev', '2-Coev'])
-box1.set_ylabel('Odsetek wygranych gier [%]')
-box1.grid(True)
-box1.set_title('Porównanie odsetku wygranych gier')
+# Ustawienia wykresu pudełkowego
+box1.boxplot(boxplot_data, showmeans=True,patch_artist=True,
+            notch=True, 
+            showfliers=False,
+            boxprops=dict(linewidth=1, edgecolor='blue',facecolor='none', color='blue', zorder=-1),
+            whiskerprops=dict(linewidth=1, color='blue', linestyle='--', dashes=(5,6)),
+            medianprops=dict(color='red', zorder=7),
+            flierprops=dict(marker='+', markeredgecolor='blue', markersize=5),
+            meanprops=dict(marker='o', markeredgecolor='black', markerfacecolor='blue', markersize=5, zorder=-2),
+)
 
-# Adjust layout and show the plots
-plt.rc('font', family='serif', serif='Times New Roman')
+#Notch -> wyciecie
+#Fliers -> wartosci odstajace
+#Whisker -> wąsy
+#Mediana ->mediana
+#Mean -> średnia
+
+# Ustawienia osi wykresu pudełkowego
+box1.set_xticklabels(['1-Evol-RS', '1-Coev-RS', '2-Coev-RS', '1-Coev', '2-Coev'], rotation=20)
+box1.set_ylim(60, 100)
+box1.tick_params(axis='x', direction='in', length=3)
+box1.tick_params(axis='y', direction='in', length=3)
+box1.xaxis.set_ticks_position('both')
+box1.yaxis.tick_right()
+box1.grid(linestyle = '--', linewidth=0.25, dashes=(7, 14))
+
+
+#ZAPIS DO PDF i Dopasowanie
 plt.tight_layout()
-plt.savefig('combined_plot.pdf')
+plt.savefig('wykres.pdf')
 plt.show()
