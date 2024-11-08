bb3fee784e7c47d5c09c3cabb37ece6a86a72abb
Piotr Nowacki <piotr.helios@gmail.com>
2024-11-08 19:00:10 +0100

    Final version of LAB2 files with ipynb and html and other unimportant stuff

--- ZAD2/zad2.py
+++ ZAD2/zad2.py
@@ -17,18 +17,19 @@ with open(file_path, 'r') as file:
 
 # Definiowanie wektora światła
 light_vector = np.array([-1, -1, 1])
-light_vector = light_vector / np.linalg.norm(light_vector)
+light_vector = light_vector / np.linalg.norm(light_vector) # dzielimy przez długość wektora = pierwiastek z sumy kwadratów
 
 # Obliczanie gradientów wysokości w kierunkach x i y
 dx, dy = np.gradient(height_map)
 
 # Tworzenie wektorów normalnych dla każdego punktu
-normals = np.dstack((-dx, -dy, np.ones_like(height_map) * distance))
+normals = np.dstack((dx, dy, np.ones_like(height_map) * distance/100 ))
 normals /= np.linalg.norm(normals, axis=2, keepdims=True)  # Normalizacja normalnych
-
 # Obliczenie współczynnika cienia (cosinus kąta między światłem a normalnymi)
-cos_angle = np.clip(np.sum(normals * light_vector, axis=2), 0, 1)  # Obcięcie do zakresu [0, 1]
-shade_factor = ((cos_angle - cos_angle.min()) / (cos_angle.max() - cos_angle.min())) ** 0.6
+cos_angle = np.sum((normals * light_vector), axis=2)  
+angle_in_radians = np.arccos(cos_angle)
+angle_in_degrees = np.degrees(angle_in_radians)
+shade_factor = ((angle_in_degrees - angle_in_degrees.min()) / (angle_in_degrees.max() - angle_in_degrees.min())) ** 0.6
 
 # Stosowanie niestandardowego gradientu do mapy wysokości
 colored_height_map = custom_cmap((height_map - height_map.min()) / (height_map.max() - height_map.min()))
@@ -44,7 +45,7 @@ hsv_map[:, :, 2] *= np.clip(shade_factor * 1.25, 0, 1)  # Zastosowanie cienia i 
 shaded_map = hsv_to_rgb(hsv_map)
 
 # Wyświetlanie wyniku
-plt.figure(figsize=(10, 8))
+plt.figure(figsize=(7, 7))
 plt.imshow(shaded_map)
 plt.title("Mapa wysokości terenu z intensywnym kolorowaniem i cieniowaniem")
 plt.xlabel("Szerokość (w punktach)")
