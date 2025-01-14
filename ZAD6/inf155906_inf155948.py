d22341956b1241e905057716eb57cc52848d8f13
Piotr Nowacki <piotr.helios@gmail.com>
2025-01-14 22:19:03 +0100

    ZAD6 final

--- ZAD6/inf155906_inf155948.py
+++ ZAD6/inf155906_inf155948.py
@@ -1,75 +1,60 @@
 import sys
 import numpy as np
 from scipy.io import wavfile
-from scipy.fft import fft, ifft
+from scipy.fft import fft
 from scipy.signal import decimate
 import warnings
 
 # Wyłączanie wszelkich ostrzeżeń
 warnings.filterwarnings("ignore")
-
 pitch_cutoff = 179  # Próg częstotliwości dla klasyfikacji płci
 
-
 def process_audio_with_hps(file_path):
-    sampling_rate, audio_signal = wavfile.read(file_path)
+    w, audio_signal = wavfile.read(file_path)
 
-    if audio_signal.ndim > 1:  # Jeśli sygnał ma więcej niż jeden kanał, weź pierwszy
+    if audio_signal.ndim >= 2:  # Jeśli sygnał ma 2 kanały lub więcej, weź pierwszy
         audio_signal = audio_signal[:, 0]
 
-    spectrum = np.abs(fft(audio_signal))
-    n = len(spectrum)
-    spectrum = spectrum[:n // 2]
-    spectrum /= n / 2
-    spectrum[0] /= 2
-    freqs = np.arange(len(spectrum)) / n * sampling_rate
+    signal1 = fft(audio_signal)
+    n = len(signal1)
+    signal1 = signal1[:n // 2]
+    signal1 = 2*np.abs(signal1)/n
+    freqs = np.arange(n)/n* w
 
+    if signal1 is None or freqs is None:
+        return None
     cutoff = -1
     for i in range(len(freqs)):
-        if freqs[i] > 2000:
+        if freqs[i] > 80:
             cutoff = i
             break
+    signal1[:cutoff] = 0
 
-    if spectrum is not None and freqs is not None:
-        low_cut_idx = -1
-        for i in range(len(freqs)):
-            if freqs[i] > 80:
-                low_cut_idx = i
-                break
-        spectrum[:low_cut_idx] = 0
+    hps_spectrum = signal1.copy()
+    for factor in range(2, 5):
+        reduced = decimate(signal1, factor)
+        hps_spectrum[:len(reduced)] *= reduced
 
-        spectrum = spectrum[:cutoff]
+    max_value = hps_spectrum[0]
+    for i in range(1, n//2):
+        if hps_spectrum[i] > max_value:
+            max_value = hps_spectrum[i]
+            max_index = i
 
-        hps_spectrum = spectrum.copy()
-        for factor in range(2, 5):
-            reduced = decimate(spectrum, factor)
-            hps_spectrum[:len(reduced)] *= reduced
-
-        max_idx = 0
-        max_value = hps_spectrum[0]
-        limit = -1
-        for i in range(len(freqs)):
-            if freqs[i] > 350:
-                limit = i
-                break
-        for i in range(1, limit):
-            if hps_spectrum[i] > max_value:
-                max_value = hps_spectrum[i]
-                max_idx = i
-
-        base_frequency = freqs[max_idx]
-        return base_frequency
-    return None
+    return freqs[max_index]
 
 def run(file_path):
-    base_frequency = process_audio_with_hps(file_path)
-    if base_frequency is None:
-        return "M"
-    else:
-        if(base_frequency < pitch_cutoff):
-            return 'M'
+    try:
+        base_frequency = process_audio_with_hps(file_path)
+        if base_frequency is None:
+            return "K"
         else:
-            return 'K'
+            if base_frequency < pitch_cutoff:
+                return 'M'
+            else:
+                return 'K'
+    except:
+        return "K"
         
 if __name__ == "__main__":
     if len(sys.argv) < 2:
